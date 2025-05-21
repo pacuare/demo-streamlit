898a5ac3dcb7d3d2cbf185e62b08bdd90d544f53
Aleks Rūtiņš <keeper@farthergate.com>
2025-05-21 01:12:42 +0000

    Put in some code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,26 @@
 import streamlit as st
+import pacuare
 
-st.title("🎈 My new app")
+db = pacuare.Client(st.secrets['PACUARE_API_KEY'])
+
+def get_data():
+  return (
+    db
+    .query("select injuries, turtle_occurrences from unique_turtles")
+    .apply(
+      lambda row: {**row, 'n_injuries': len(row['injuries'].split(','))},
+      axis=1,
+      result_type='expand'
+    )
+  )
+
+st.title("🎈 Pacuare Demo")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "See below a scatter plot of turtle occurrences vs. injuries."
+)
+
+st.scatter_chart(
+  data=get_data(),
+  x='turtle_occurrences',
+  y='n_injuries'
 )
